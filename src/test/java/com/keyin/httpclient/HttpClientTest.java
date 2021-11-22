e0a06979e25937570d80d4ccb06ca557ba9ebeab
CameronBrenton <Cameron.Brenton@keyin.com>
2021-11-22 14:24:09 -0330

    First commit

---
+++ src/test/java/com/keyin/httpclient/HttpClientTest.java
@@ -0,0 +1,15 @@
+package com.keyin.httpclient;
+
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class HttpClientTest {
+    public static void main(String[] args){
+
+    }
+}
